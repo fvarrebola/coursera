e370a69a7bde1522532bdd25146750566bcd1429
Fabio Arrebola <fvarrebola@gmail.com>
2013-01-23 23:11:19 -0200

    week1 first scripts

---
+++ image-video-processing/week1/density.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot
+from scipy import ndimage
+import numpy
+from numpy.ma.core import floor
+
+print ('********************************************************************************')
+print ('* Changing the intensity levels of an image using SciPy, NumPy and MatPlotLib  *')
+print ('********************************************************************************')
+
+fpath = raw_input('> image\'s full path:')
+try:
+   with open(fpath) as f: pass
+except IOError as e:
+   print (fname + ' could not be opened')
+
+# reads the image using ndimage.imread()...
+print ('\treading image...')
+img = ndimage.imread(fpath)
+img_array = numpy.array(img)
+
+# builds new images...
+intensities = [2, 4, 8, 16, 32, 64, 128, 255];
+for intensity in intensities: 
+    print ('\tbuilding new image using an intensity level of \'' + str(intensity) + '\'...')
+    new_img_array = floor(numpy.array(img_array) / intensity) * intensity
+    new_fpath = fpath + '-' + str(intensity) + '.png'
+    print ('\t\tsaving new image to \'' + new_fpath + '\'...')
+    pyplot.imsave(new_fpath, new_img_array, cmap='gray', format='png')
+
+print ('********************************************************************************')
---
+++ image-video-processing/week1/histogram.py
@@ -0,0 +1,41 @@
+from scipy import ndimage
+import numpy
+import pylab
+
+print ('********************************************************************************')
+print ('* Building a histogram using SciPy, NumPy and Pylab                            *')
+print ('********************************************************************************')
+
+fpath = raw_input('> image\'s full path:')
+try:
+   with open(fpath) as f: pass
+except IOError as e:
+   print (fname + ' could not be opened')
+
+# reads the image using ndimage.imread()...
+print ('\treading image...')
+img = ndimage.imread(fpath)
+img_array = numpy.array(img)
+
+# builds the histogram using ndimage.histogram()...
+print ('\tbuilding histogram...')
+bins_count = 256
+histogram = ndimage.histogram(img_array, 0, 255, bins_count)
+
+# plots the histogram using pylab...
+print ('\tploting histogram...')
+x_label = 'pixel'
+y_label = 'number of pixels'
+pylab.plot(histogram)
+pylab.axis(xmax=bins_count)
+pylab.axes().set_xlabel(x_label)
+pylab.axes().set_ylabel(y_label)
+
+hpath = fpath + '-histogram.png'
+print ('\tsaving histogram to file \'' + hpath + '\'...')
+pylab.savefig(fpath + '-histogram.png', format='png')
+
+print ('\tshowing histogram...')
+pylab.show()
+
+print ('********************************************************************************')
