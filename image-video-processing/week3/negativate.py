b15d00f1cf688f2114feae6498c83e19e352c336
Fabio Arrebola <fvarrebola@gmail.com>
2013-02-03 20:56:29 -0200

    Replacing 'abs(array - 255)' with '(255 - array)'

--- image-video-processing/week3/negativate.py
+++ image-video-processing/week3/negativate.py
@@ -10,7 +10,7 @@ import userinput
 fpath = userinput.get_img_path()
 img_array = userinput.get_gray_img(fpath)
 (img_array_x_len, img_array_y_len) = img_array.shape
-new_img_array = abs(img_array - 255)
+new_img_array = 255 - img_array
 new_path = fpath + '.grayscale-negative.bmp'
 print 'saving grayscale negative image as \'%s\'...' % (new_path)
 imsave(new_path, new_img_array)
