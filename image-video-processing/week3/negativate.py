4d0a0ac96911ba4b6b83e9555a8cf34884177d22
Fabio Arrebola <fvarrebola@gmail.com>
2013-02-03 20:51:27 -0200

    Week 3 negativate script

---
+++ image-video-processing/week3/negativate.py
@@ -0,0 +1,17 @@
+from scipy.misc.pilutil import imsave
+import numpy
+import sys
+
+print ('********************************************************************************')
+print ('* Really simple negative image                                                 *')
+print ('********************************************************************************')
+sys.path.append('../utils')
+import userinput
+fpath = userinput.get_img_path()
+img_array = userinput.get_gray_img(fpath)
+(img_array_x_len, img_array_y_len) = img_array.shape
+new_img_array = abs(img_array - 255)
+new_path = fpath + '.grayscale-negative.bmp'
+print 'saving grayscale negative image as \'%s\'...' % (new_path)
+imsave(new_path, new_img_array)
+print ('********************************************************************************')
